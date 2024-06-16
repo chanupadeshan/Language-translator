ed948443d67b1c974d606c1a7fe484260ee8a23e
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-06-16 16:42:55 +0530

    create get language code from language_code

--- Translator.py
+++ Translator.py
@@ -1,4 +1,14 @@
+
 from translate import Translator
+from language_code import language_codes
+
+def get_language_code(language_name):
+    return language_codes.get(language_name.lower())
+
+
+form_lang = 'english'
+to_lang = 'spanish'
+
 translator = Translator(from_lang='en',to_lang='es')
 translation = translator.translate("Hello")
 print(translation)  # Output: Hola

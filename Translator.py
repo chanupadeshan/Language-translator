447fc0aefd2633bf0bd0fd69983c852336e1c30f
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-06-16 17:11:16 +0530

    create labels and combobox for the get from language and to language

--- Translator.py
+++ Translator.py
@@ -12,17 +12,32 @@ def get_language_code(language_name):
     return language_codes.get(language_name.lower())
 
 
-form_lang = input("Enter the language you want to translate from: ")
-to_lang = input("Enter the language you want to translate to: ")
+from_lang = tk.StringVar()
+to_lang = tk.StringVar()
 
-if form_lang not in language_codes.keys() or to_lang not in language_codes.keys():
-    print("Invalid language")
-    exit()
-else:
-    translate_word = input("Enter the word you want to transalte:")
-    translator = Translator(from_lang=get_language_code(form_lang),to_lang=get_language_code(to_lang))
-    translation = translator.translate(translate_word)
-    print(translation)  
+from_lang_label = ttk.Label(root,text="Enter the language you want to translate from: ",font=("Helvetica",16),foreground="green")
+from_lang_label.pack()
+
+from_lang_combobox = ttk.Combobox(root,values=list(language_codes.keys()),textvariable=from_lang)
+from_lang_combobox.pack()
+
+to_lang_labal = ttk.Label(root,text="Enter the language you want to translate to: ",font=("Helvetica",16),foreground="green")
+to_lang_labal.pack()
+
+to_lang_combobox = ttk.Combobox(root,values=list(language_codes.keys()),textvariable=to_lang)
+to_lang_combobox.pack()
+
+# form_lang = input("Enter the language you want to translate from: ")
+# to_lang = input("Enter the language you want to translate to: ")
+
+# if form_lang not in language_codes.keys() or to_lang not in language_codes.keys():
+#     print("Invalid language")
+#     exit()
+# else:
+#     translate_word = input("Enter the word you want to transalte:")
+#     translator = Translator(from_lang=get_language_code(form_lang),to_lang=get_language_code(to_lang))
+#     translation = translator.translate(translate_word)
+#     print(translation)  
 
 root.mainloop()
 

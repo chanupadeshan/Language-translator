e3dc814123b30f0a2cdf13cb06b3ab6859273b22
Chanupa deshan <chanupadeshan2002@gmail.com>
2024-06-16 16:56:10 +0530

    correct some errors

--- Translator.py
+++ Translator.py
@@ -9,11 +9,11 @@ def get_language_code(language_name):
 form_lang = input("Enter the language you want to translate from: ")
 to_lang = input("Enter the language you want to translate to: ")
 
-if form_lang not in language_codes.keys() & to_lang not in language_codes.key():
+if form_lang not in language_codes.keys() or to_lang not in language_codes.keys():
     print("Invalid language")
     exit()
 else:
-    translate_word = inoput("Enter the word you want to transalte:")
+    translate_word = input("Enter the word you want to transalte:")
     translator = Translator(from_lang=get_language_code(form_lang),to_lang=get_language_code(to_lang))
     translation = translator.translate(translate_word)
     print(translation)  
